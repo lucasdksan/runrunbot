26303340afd7f010e094c071d3cb124304e7b0dd
lucasdksan <lucas.leoncio.silva@gmail.com>
2025-07-30 12:47:36 -0300

    feat: camada de infraestrutura/repositorio

---
+++ src/users/infrastructure/database/sqlite/models/user-model.mapper.ts
@@ -0,0 +1,25 @@
+import { ValidationError } from "../../../../../shared/domain/errors/validation-error";
+import { UserEntity } from "../../../../domain/entities/user.entity";
+
+export type UserModelMapperInput = {
+    id: string;
+    discordUser: string;
+    runrunitUser: string;
+    createdAt: string;
+};
+
+export class UserModelMapper {
+    static toEntity(model: UserModelMapperInput){
+        const data = {
+            discordUser: model.discordUser,
+            runrunitUser: model.runrunitUser,
+            createdAt: new Date(model.createdAt)
+        };
+
+        try {
+            return new UserEntity(data, model.id);
+        } catch(err) {
+            throw new ValidationError("An entity not be loaded");
+        }
+    }
+}
