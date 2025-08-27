1da7c98e1f3873e33c88d95d6a789672b7142f94
lucasdksan <lucas.leoncio.silva@gmail.com>
2025-08-27 10:20:15 -0300

    fix: implementação de interfaces para o DiscordServices

---
+++ src/shared/infrastructure/discord/repositories/i-discord-repository.ts
@@ -0,0 +1,7 @@
+import { ChannelInputDto } from "../dtos/channel-input.dto";
+import { MessageInputDto } from "../dtos/message-input.dto";
+
+export interface IDiscordRepository {
+    sendDM(dto: MessageInputDto): Promise<void>
+    sendChannel(dto: ChannelInputDto): Promise<void>
+}
